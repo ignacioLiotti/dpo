e0aaa50d81b608cb61e01b322ea33d3bb8c0b9cf
ignacioLiotti <ignacioliotti@zequenze.com>
2025-05-06 15:37:11 -0300

    default event callendar

---
+++ app/(sidebar)/calendar/page.tsx
@@ -0,0 +1,13 @@
+import CalendarComp from "@/components/comp-542";
+
+const page = () => {
+  return (
+    <div>
+      Calendar
+      <CalendarComp />
+    </div>
+  );
+};
+
+export default page;
+
